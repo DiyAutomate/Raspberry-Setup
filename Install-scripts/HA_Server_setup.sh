b8d52caae1ae616a443bd1aba6d9676fb59a25d9
PaulBeauvais <Paul@diyAutomate.me>
2018-06-03 01:42:03 -0400

    updated service path

--- Install-scripts/HA_Server_setup.sh
+++ Install-scripts/HA_Server_setup.sh
@@ -35,19 +35,19 @@ sudo pip3 install sqlalchemy
 sudo pip3 install --upgrade homeassistant
 
     # if service does not auto install uncomment these lines otherwise comment them out.
-sudo touch /etc/systemd/system/home-assistant@hass.service
+sudo touch /etc/systemd/system/hass.service
 
-sudo echo "[Unit]" > /etc/systemd/system/home-assistant@hass.service
-sudo echo "Description=Home Assistant" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "After=network.target" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo " " >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "[Service]" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "Type=simple" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "User=pi" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "ExecStart=/usr/local/bin/hass" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo " ">> /etc/systemd/system/home-assistant@hass.service
-sudo echo "[Install]" >> /etc/systemd/system/home-assistant@hass.service
-sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "[Unit]" > /etc/systemd/system/hass.service
+sudo echo "Description=Home Assistant" >> /etc/systemd/system/hass.service
+sudo echo "After=network.target" >> /etc/systemd/system/hass.service
+sudo echo " " >> /etc/systemd/system/hass.service
+sudo echo "[Service]" >> /etc/systemd/system/hass.service
+sudo echo "Type=simple" >> /etc/systemd/system/hass.service
+sudo echo "User=pi" >> /etc/systemd/system/hass.service
+sudo echo "ExecStart=/usr/local/bin/hass" >> /etc/systemd/system/hass.service
+sudo echo " ">> /etc/systemd/system/hass.service
+sudo echo "[Install]" >> /etc/systemd/system/hass.service
+sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/hass.service
 
 sudo systemctl --system daemon-reload
 sudo systemctl enable hass.service
