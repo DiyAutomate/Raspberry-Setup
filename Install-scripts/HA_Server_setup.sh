06c32cbd5dc7702bb369c2ecd934bcf8e230ae67
PaulBeauvais <Paul@diyAutomate.me>
2018-06-03 01:06:36 -0400

    add service file, sqlalchamy, and reboot

--- Install-scripts/HA_Server_setup.sh
+++ Install-scripts/HA_Server_setup.sh
@@ -6,7 +6,9 @@ echo "updating and upgrading the system"
     #Update and upgrade the system
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt autoremove -y
 
+sudo 
     #Install Python
     #not needed on current build
 #sudo apt-get install pyton3 python3-pip
@@ -26,7 +28,26 @@ apt-get install git
 
 echo "installing home assistant"
   #install home assistant
-sudo pip3 install homeassistant
+  #may need a sqlalchemy install
+sudo pip3 install sqlalchemy
+sudo pip3 install --upgrade homeassistant
+
+sudo touch /etc/systemd/system/home-assistant@hass.service
+
+sudo echo "[Unit]" > /etc/systemd/system/home-assistant@hass.service
+sudo echo "Description=Home Assistant" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "After=network.target" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo " " >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "[Service]" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "Type=simple" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "User=pi" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "ExecStart=/usr/local/bin/hass" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo " ">> /etc/systemd/system/home-assistant@hass.service
+sudo echo "[Install]" >> /etc/systemd/system/home-assistant@hass.service
+sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/home-assistant@hass.service
+
+sudo systemctl --system daemon-reload
+sudo systemctl enable hass.service
 
 echo "setting up home assistant aliases"
   #setup home assistant service aliases (to make life easier)
@@ -43,3 +64,5 @@ echo " "
 
 echo "please change your password by typing passwd"
 echo "please change the root password by typing passwd root"
+
+sudo reboot
