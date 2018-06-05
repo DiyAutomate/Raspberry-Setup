8df873773863a3c2fca16f1660362acb09305666
PaulBeauvais <Paul@diyAutomate.me>
2018-06-05 00:16:21 -0400

    full reconfigure for automated profile rename and install

--- Install-scripts/HA_Server_setup.sh
+++ Install-scripts/HA_Server_setup.sh
@@ -1,66 +1,82 @@
 #!/bin/bash
 
+#step one update /etc/rc.local to change the user name
+    #delete the exit code so we can move it to the end.
+sudo sed -i /'exit 0'/d /etc/rc.local
+    #write out needed lines
+    # 1 - change the user name (copies user profile)
+sudo echo "usermod -l haadmin -d /home/haadmin -m pi" >> /etc/rc.local
+    # 2 - update sudoers so that sudo works the way we want (does not ask for password)
+sudo echo "mv /etc/sudoers.d/010_pi-nopasswd /etc/sudoers.d/010_haadmin-nopasswd" >> /etc/rc.local
+
+# step two write out install script 
+# this could be a file to copy, but I like having everything in one place.
+
+    # create script file
+touch SystemUpdate.sh
+
 # to run first chmod a+x AH_Server_setup.sh
 
-echo "updating and upgrading the system"
+echo "echo 'updating and upgrading the system'" >> .SystemUpdate.sh
     #Update and upgrade the system
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt autoremove -y
-
-sudo 
+echo "sudo apt-get update" >> .SystemUpdate.sh
+echo "sudo apt-get upgrade -y" >> .SystemUpdate.sh
+echo "sudo apt autoremove -y" >> .SystemUpdate.sh
+ 
     #Install Python
     #not needed on current build
 #sudo apt-get install pyton3 python3-pip
 
 echo "installing rmate"
    #install rmate
-sudo wget -O /usr/local/bin/rmate https://raw.github.com/aurora/rmate/master/rmate
-chmod a+x /usr/local/bin/rmate
+sudo wget -O /usr/local/bin/rmate https://raw.github.com/aurora/rmate/master/rmate" >> .SystemUpdate.sh
+sudo chmod a+x /usr/local/bin/rmate" >> .SystemUpdate.sh
 
-echo "adding rmate alias to .bashrc"
+echo 'adding rmate alias to .bashrc'" >> .SystemUpdate.sh
    #start rmate when current user logs on
-echo "alias code='rmate -p 52698'" >> .bashrc
+echo 'alias code='rmate -p 52698'' >> .bashrc" >> .SystemUpdate.sh
 
-echo "installing git" 
+echo 'installing git'" >> .SystemUpdate.sh 
   #install git
-apt-get install git
+apt-get install git" >> .SystemUpdate.sh
 
-echo "installing home assistant"
+echo "installing home assistant'" >> .SystemUpdate.sh
   #install home assistant
   
     #may need a sqlalchemy install
     #some installations do not have a valid version of sqlalchemy reinstalling will not hurt
-sudo pip3 install sqlalchemy
-sudo pip3 install --upgrade homeassistant
+sudo pip3 install sqlalchemy" >> .SystemUpdate.sh
+sudo pip3 install --upgrade homeassistant" >> .SystemUpdate.sh
 
     # if service does not auto install uncomment these lines otherwise comment them out.
-sudo touch /etc/systemd/system/hass.service
+sudo touch /etc/systemd/system/hass.service" >> .SystemUpdate.sh
 
-sudo echo "[Unit]" > /etc/systemd/system/hass.service
-sudo echo "Description=Home Assistant" >> /etc/systemd/system/hass.service
-sudo echo "After=network.target" >> /etc/systemd/system/hass.service
-sudo echo " " >> /etc/systemd/system/hass.service
-sudo echo "[Service]" >> /etc/systemd/system/hass.service
-sudo echo "Type=simple" >> /etc/systemd/system/hass.service
-sudo echo "User=pi" >> /etc/systemd/system/hass.service
-sudo echo "ExecStart=/usr/local/bin/hass" >> /etc/systemd/system/hass.service
-sudo echo " ">> /etc/systemd/system/hass.service
-sudo echo "[Install]" >> /etc/systemd/system/hass.service
-sudo echo "WantedBy=multi-user.target" >> /etc/systemd/system/hass.service
+sudo echo "sudo echo '[Unit]' > /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'Description=Home Assistant' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'After=network.target' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo ' ' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo '[Service]' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'Type=simple' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'User=haadmin' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'ExecStart=/usr/local/bin/hass" >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo ' '>> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo '[Install]' >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
+sudo echo "sudo echo 'WantedBy=multi-user.target" >> /etc/systemd/system/hass.service" >> .SystemUpdate.sh
 
-sudo systemctl --system daemon-reload
-sudo systemctl enable hass.service
+sudo echo "sudo systemctl --system daemon-reloadsudo echo "
+sudo echo "sudo systemctl enable hass.service" >> .SystemUpdate.sh
 
     #stop commenting for service installation.
 
-echo "setting up home assistant aliases"
+sudo echo "echo 'setting up home assistant aliases'" >>SystemUpdate.sh
   #setup home assistant service aliases (to make life easier)
-echo "alias hrestart='sudo systemctl restart hass'" >> .bashrc
-echo "alias hstop='sudo systemctl stop hass'" >> .bashrc
-echo "alias hstart='sudo systemctl start hass'" >> .bashrc
+sudo echo "echo 'alias hrestart='sudo systemctl restart hass'' >> .bashrc" >> .SystemUpdate.sh
+sudo echo "echo 'alias hstop='sudo systemctl stop hass'' >> .bashrc" >> .SystemUpdate.sh
+sudo echo "echo 'alias hstart='sudo systemctl start hass'' >> .bashrc" >> .SystemUpdate.sh
 
+echo "sudo bash SystemUpdate.sh" >> .bashrc
 
+echo "sudo sed -i /'usermod -l haadmin -d \/home\/haadmin \-m pi'/d etc/rc.local"
 
 echo "Installations complete"
 echo "no error checking done"
